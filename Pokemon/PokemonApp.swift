c6df2a625a31486a51e64fab892e9fd8d131949a
pntrgbr <pintergabor1990@gmail.com>
2024-09-16 15:23:08 +0200

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Gabor Pinter on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
